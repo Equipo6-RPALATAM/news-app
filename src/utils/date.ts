2bd79850d92fd08244137b9ca168c8d4cd58cbc8
Daniel <dyangeltk17@gmail.com>
2023-02-08 09:21:42 -0500

    feat: :construction: Add new function for date formatting

---
+++ src/utils/date.ts
@@ -0,0 +1,11 @@
+const months: Array<string> = ['Januay', 'February', 'March', 'April', 'May']
+
+export const dateTransform = (date: string): string => {
+  const transformedDate = new Date(date)
+
+  const year = transformedDate.getFullYear()
+  const month = transformedDate.getMonth()
+  const day = transformedDate.getDay()
+
+  return `${months[month]} ${day}, ${year}`
+}
