5b4589dced01fde2249b160d274f44fb09fd79af
Daniel <dyangeltk17@gmail.com>
2023-02-08 09:23:17 -0500

    feat: :construction: Add header and footer components

---
+++ src/shared/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = (): JSX.Element => {
+  return (
+    <footer className="footer">
+      <h5 className="footer__description">Dayli - Al right reserved</h5>
+    </footer>
+  )
+}
